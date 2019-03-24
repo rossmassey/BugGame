bfff786d1d4eacfedf2a07f5bd2f53dc32c7a994
Ross Massey <rmassey@ucsd.edu>
2019-03-23 19:44:52 -0700

    Add bug particles to infestation object

--- Assets/Scripts/InfestationObject.cs
+++ Assets/Scripts/InfestationObject.cs
@@ -1,27 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InfestationObject : MonoBehaviour
 {
+    [Header("Infestation")]
     [SerializeField] float maxInfestation = 100f;
-
     [SerializeField] Color cleanColor;
     [SerializeField] Color infestedColor;
 
-    float currentInfestation = 100;
+    [Header("Bug Particles")]
+    [SerializeField] ParticleSystem bugEffectToUse;
+    [SerializeField] float maxBugParticles = 50f;
+    // TODO make bugs bounce more
 
+    float currentInfestation = 100;
     Renderer rend;
+
+    ParticleSystem bugEffect;
+    ParticleSystem.MainModule bugEffectSettings;
+    bool bugEffectPlaying = true;
 
 
     void Start()
     {
         rend = GetComponent<Renderer>();
+        bugEffect = Instantiate(bugEffectToUse, transform);
+        bugEffectSettings = bugEffect.main;
     }
 
     void Update()
     {
         ColorByInfestation();
+        ManageParticles();
+    }
+
+    private void ManageParticles()
+    {
+        if (bugEffectPlaying)
+        {
+            bugEffectSettings.maxParticles = Mathf.RoundToInt(InfestationPercent() * maxBugParticles);
+        }
+        
+        if (bugEffectPlaying && currentInfestation == 0)
+        {
+            // TODO figure out why some bugs are left over
+            bugEffect.Stop();
+            bugEffectPlaying = false;
+        }
+        
+        if (!bugEffectPlaying && currentInfestation > 1)
+        {
+            bugEffect.Play();
+            bugEffectPlaying = true;
+        }
     }
 
     public void AddInfestation(float amount)
